14d7a00a903265f7a0c11b182833812544819d00
Avery Miller <averymiller2060@gmail.com>
2023-09-23 19:19:32 -0400

    implemented js logic to store each project into one object variable

---
+++ src/projects.js
@@ -0,0 +1,51 @@
+import image from './assets/images/voyager.png';
+
+const projects = [
+  {
+    name: "Voyager",
+    methods: "HTML/CSS/JavaScript",
+    src: "",
+    img: {
+      src: image,
+      alt: ""
+    }
+  },
+  {
+    name: "U-Hub",
+    methods: "HTML/CSS/JavaScript",
+    src: "",
+    img: {
+      src: image,
+      alt: ""
+    }
+  },
+  {
+    name: "Weather Dashboard",
+    methods: "HTML/CSS/JavaScript",
+    src: "",
+    img: {
+      src: image,
+      alt: ""
+    }
+  },
+  {
+    name: "Work Day Scheduler",
+    methods: "HTML/CSS/JavaScript",
+    src: "",
+    img: {
+      src: image,
+      alt: ""
+    }
+  },
+  {
+    name: "Code Quiz",
+    methods: "HTML/CSS/JavaScript",
+    src: "",
+    img: {
+      src: image,
+      alt: ""
+    }
+  }
+];
+
+export default projects;
