108cb2952dec67e29e49bbb2f3aa11e649cce4cf
Avery Miller <averymiller2060@gmail.com>
2023-09-23 19:18:45 -0400

    implemented the react router to handle different path requests

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import App from './App'
+import AboutPage from './pages/AboutPage';
+import WorkPage from './pages/WorkPage';
+// import ContactPage from './pages/ContactPage';
+// import ResumePage from './pages/ResumePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'work',
+        element: <WorkPage />
+      },
+      // {
+      //   path: 'contact',
+      //   element: <ContactPage />
+      // },
+      // {
+      //   path: 'resume',
+      //   element: <ResumePage />
+      // }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
