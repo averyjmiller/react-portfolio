34b18c88a40692107337ba7a20bc2aa756c2511b
Avery Miller <averymiller2060@gmail.com>
2023-09-24 14:18:52 -0400

    uncommented the contact and resume paths for the router

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+// import './index.css';
 
 import App from './App'
 import AboutPage from './pages/AboutPage';
 import WorkPage from './pages/WorkPage';
-// import ContactPage from './pages/ContactPage';
-// import ResumePage from './pages/ResumePage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
 
 const router = createBrowserRouter([
   {
@@ -23,14 +23,14 @@ const router = createBrowserRouter([
         path: 'work',
         element: <WorkPage />
       },
-      // {
-      //   path: 'contact',
-      //   element: <ContactPage />
-      // },
-      // {
-      //   path: 'resume',
-      //   element: <ResumePage />
-      // }
+      {
+        path: 'contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />
+      }
     ]
   }
 ])
