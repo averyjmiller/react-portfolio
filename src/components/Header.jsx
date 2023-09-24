2de819f83b3296abb58a0e6950629bca8b769ba2
Avery Miller <averymiller2060@gmail.com>
2023-09-24 12:21:43 -0400

    added a header that is wrapped around the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from './Navigation';
 
 export default function Header() {
   return (
-    <header id="header">
+    <header id="nav-header">
       <h1>Avery Miller</h1>
       <Navigation />
     </header>
