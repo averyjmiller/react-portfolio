6c83a9e126d83f26b35b16c81f89187db4fc482c
Avery Miller <averymiller2060@gmail.com>
2023-09-24 12:21:17 -0400

    added links to personal mail, linkedin, and github in footer with custom icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
   return (
     <footer>
-      <p>Thanks for checking out my portfolio!</p>
+      <a href="mailto:averymiller2060@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+      <a href="https://www.linkedin.com/in/avery-j-miller" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+      <a href="https://github.com/averyjmiller" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
     </footer>
   );
 }
