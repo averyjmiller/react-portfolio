d9d249493e3769c9fadd0b727a28575c2971b02b
Avery Miller <averymiller2060@gmail.com>
2023-09-25 16:40:16 -0400

    updated the info on the About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,20 +6,21 @@ export default function AboutPage() {
       <section className="page-section" id="about">
         <div id="about-content">
           <header>
-            <h1>Hello! I'm Avery, a full stack web developer.</h1>
+            <h1>About Me</h1>
+            <div id="line-break"></div>
           </header>
           <div id="about-image-sm">
             <img className="profile-image" src={profileImage} alt="Avery Miller"/>
           </div>
           <p>
+            Hello! I'm Avery, a full stack web developer.
+          </p>
+          <p>
             I am currently studying through The Ohio State University's coding boot camp. 
             I am passionate about creating accessible and fluid web apps with an innovative back-end!
           </p>
           <p>
-            My technical skills include HTML5, CSS3, JavaScript, jQuery, REST APIs, Node, and Express.
-          </p>
-          <p>
-            I am currently learning SQL to develop my skills in databases.
+            My technical skills include--but are not limited to--methods in the MERN stack. Check out my proficiencies on the <a href='./resume'>resume</a> page.
           </p>
           <p>
             If you have any questions or inquiries, please feel free to contact me. I appreciate your time for checking out my portfolio, and I hope to hear from you!
