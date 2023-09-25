baa76ba17fbaf509ece5b5d48bc4d4dd2ef74ca1
Avery Miller <averymiller2060@gmail.com>
2023-09-25 16:41:10 -0400

    added a downloadable pdf of my resume and included my proficiencies on the resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -3,7 +3,37 @@ import resumeFile from '../assets/files/Avery_Miller_Resume.pdf';
 export default function ResumePage() {
   return (
     <section className="page-section" id="resume">
-      <a href={resumeFile} target="_blank" download="Avery_Miller_Resume">Download my resume!</a>
+      <header>
+        <h1>Resume</h1>
+        <div id="line-break"></div>
+        <p>Download my resume <a href={resumeFile} target="_blank" download="Avery_Miller_Resume">here</a></p>
+      </header>
+      <section id="proficiencies">
+        <section id="front-end">
+          <h2>Front-end Proficiencies</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </section>
+        <section id="back-end">
+          <h2>back-end Proficiencies</h2>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </section>  
+      </section>
     </section>
   );
 }
