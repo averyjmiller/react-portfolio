875afda40da62ec8b0abe9a30ef73714e37bfad3
Avery Miller <averymiller2060@gmail.com>
2024-05-02 23:57:33 -0400

    implemented App functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './reset.css';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import Navigation from './components/Navigation';
+import MobileNavigation from './components/MobileNavigation';
 import Footer from './components/Footer';
 
 function App() {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  )
+
+  useEffect(() => {
+    window
+    .matchMedia("(min-width: 768px)")
+    .addEventListener('change', e => setMatches(e.matches));
+  }, []);
+
   return (
     <>
-      <Header />
+      {matches && (<Navigation />)}
+      {!matches && (<MobileNavigation />)}
       <Outlet />
       <Footer />
     </>
