5c3020ab9da59d424f3721d670d89ddd16247e89
Avery <averymiller2060@gmail.com>
2025-05-31 21:17:21 -0400

    added custom layout on Home and About pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,32 @@
 import './AboutPage.css';
 
+import aboutPicture from '../assets/images/avery_miller_2.jpg';
+
 export default function AboutPage() {
   return (
     <div className='wrapper'>
+      <div className="container-fluid">
+        <div className="row justify-content-between">
+          <div className="col-12 col-md-6 pb-5">
+            <h1>
+              Hey! I'm Avery, a full stack web developer.
+            </h1>
+            <p>
+              I am a graduate of the Full-Stack Flex Coding Boot Camp program through Ohio State University.
+              I am passionate about creating accessible and fluid web apps with an innovative back-end!
+              <br/>
+              <br/>
+              My technical skills include--but are not limited to--methods in the MERN stack. Check out my proficiencies on the resume page.
+              <br/>
+              <br/>
+              If you have any questions or inquiries, please feel free to contact me. I appreciate your time for checking out my portfolio, and I hope to hear from you!
+            </p>
+          </div>
+          <div className="col-12 col-md-5 text-center">
+            <img src={aboutPicture} id="aboutPicture" alt="Avery Miller" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,21 @@
 import './HomePage.css';
 
-import homepageProfile from '../assets/images/homepage_profile.jpg';
+import homePicture from '../assets/images/avery_miller_1.jpeg';
 
 export default function HomePage() {
   return (
     <div className="wrapper">
       <div className="container-fluid">
         <div className="row justify-content-between">
-          <div className="col-12 order-sm-2 col-sm-6 pb-5">
-            <h1>
-              Hey! I'm Avery, a full stack web developer.
+          <div className="col-12 col-md-6 pb-5 d-flex align-items-center">
+            <h1 id="home-header">
+              <div>Hi,</div>
+              <div>I'm <span id="avery-text">Avery</span></div>
+              <div>Full Stack Developer</div>
             </h1>
-            <p>
-              I am a graduate of the Full-Stack Flex Coding Boot Camp program through Ohio State University.
-              I am passionate about creating accessible and fluid web apps with an innovative back-end!
-              <br/>
-              <br/>
-              My technical skills include--but are not limited to--methods in the MERN stack. Check out my proficiencies on the resume page.
-              <br/>
-              <br/>
-              If you have any questions or inquiries, please feel free to contact me. I appreciate your time for checking out my portfolio, and I hope to hear from you!
-            </p>
           </div>
-          <div className="col-12 order-sm-1 col-sm-5 text-center">
-            <img src={homepageProfile} id="homepageProfile" alt="Avery Miller" />
+          <div className="col-12 col-md-5 text-center">
+            <img src={homePicture} id="homePicture" alt="Avery Miller" />
           </div>
         </div>
       </div>
